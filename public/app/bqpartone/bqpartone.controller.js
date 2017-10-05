10b383a61a1096c1b173e702e843cc7ca9217389
Dmitry Kuznetsov <dmitry.kuznetsov@maxusglobal.com>
2017-10-05 12:48:26 +0300

    Made part of filters

--- public/app/bqpartone/bqpartone.controller.js
+++ public/app/bqpartone/bqpartone.controller.js
@@ -2,7 +2,23 @@
 
 angular.module('bqpartone').controller('preResultTable', ['$scope', 'bqpartoneFactory',
     ($scope, bqpartoneFactory) => {
-        let getResults = () => {
+        $scope.showDropdown =  (eventTarget) => {
+            if (eventTarget.className != 'customReportBoxDropdownButton') {
+                eventTarget = eventTarget.closest('.customReportBoxDropdownButton');
+            }
+			eventTarget.classList.toggle('cRBDBPActive');
+            hideShowBoxes(eventTarget.nextElementSibling);
+            document.addEventListener('click', function hideDrop(e) {				
+                if (e.target.closest('.customReportBoxMainArea')!= eventTarget.parentNode) {                 
+                    hideShowBoxes(eventTarget.nextElementSibling);
+					eventTarget.classList.toggle('cRBDBPActive');
+                    document.removeEventListener('click', hideDrop);
+                }
+            });
+        };
+
+		
+		let getResults = () => {
             bqpartoneFactory.getResultsForTable()
                 .then((data)=>{
                     killLoader();
@@ -11,14 +27,19 @@ angular.module('bqpartone').controller('preResultTable', ['$scope', 'bqpartoneFa
                     $scope.tableContent.splice(0,1);
                 });
         };
-
+		
+		let hideShowBoxes = (currentBox) =>{
+			console.log(currentBox);
+            currentBox.classList.toggle('hideElement');
+        };
+		
         let killLoader = () => {
             document.getElementsByClassName('loaderDiv')[0].innerHTML = '';
 			document.getElementsByClassName('tableAppSection')[0].classList.remove('hideElement');
-        }
+        };
 
             getResults();
 
-
+		
 
     }]);
