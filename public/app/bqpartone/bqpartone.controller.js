d3fac7d53305d8fe7acdcfde4bac0caf904a955b
Dmitry Kuznetsov <dmitry.kuznetsov@maxusglobal.com>
2017-10-06 16:21:27 +0300

    FilterMenu done with ngrepeat and autofill

--- public/app/bqpartone/bqpartone.controller.js
+++ public/app/bqpartone/bqpartone.controller.js
@@ -2,6 +2,29 @@
 
 angular.module('bqpartone').controller('preResultTable', ['$scope', 'bqpartoneFactory',
     ($scope, bqpartoneFactory) => {
+		$scope.menuElements = {
+			data_source: {
+				name: 'Источники',
+				content : ['Google Analytics', 'Yandex.Metrika', 'PostBuy', 'Tns']
+			},
+			Campaign : {
+				name: 'Кампании',
+				content: []
+			},
+			Placement : {
+				name: 'Размещение',
+				content: []
+			},
+			Medium : {
+				name: 'Medium',
+				content: []
+			},
+			Format : {
+				name: 'Формат',
+				content: []
+			}
+		};
+		
         $scope.showDropdown =  (eventTarget) => {
             if (eventTarget.className != 'customReportBoxDropdownButton') {
                 eventTarget = eventTarget.closest('.customReportBoxDropdownButton');
@@ -21,15 +44,29 @@ angular.module('bqpartone').controller('preResultTable', ['$scope', 'bqpartoneFa
 		let getResults = () => {
             bqpartoneFactory.getResultsForTable()
                 .then((data)=>{
-                    killLoader();
+					console.log(data.data);
                     $scope.tableContentHeader = data.data[0];
-                    $scope.tableContent = data.data;
-                    $scope.tableContent.splice(0,1);
+                    $scope.tableContent = data.data.slice(1);
+					fillMenuElements($scope.tableContentHeader, $scope.tableContent);
+					killLoader();
                 });
         };
 		
+		let fillMenuElements = (columnNames, tableData) => {
+			let reqNames = [columnNames.indexOf('Campaign'), columnNames.indexOf('Placement'), columnNames.indexOf('Medium'), columnNames.indexOf('Format')];
+			tableData.forEach((row)=>{
+				reqNames.forEach((indx)=>{
+					row[indx].split(',').forEach((elem)=>{
+						if ($scope.menuElements[columnNames[indx]].content.indexOf(elem) == -1) {
+							$scope.menuElements[columnNames[indx]].content.push(elem);
+						}
+					});
+				});
+			});
+		};
+		
+		
 		let hideShowBoxes = (currentBox) =>{
-			console.log(currentBox);
             currentBox.classList.toggle('hideElement');
         };
 		
@@ -39,7 +76,7 @@ angular.module('bqpartone').controller('preResultTable', ['$scope', 'bqpartoneFa
 			document.getElementsByClassName('menuSection')[0].classList.remove('hideElement');
         };
 
-            getResults();
+		getResults();
 
 		
 
