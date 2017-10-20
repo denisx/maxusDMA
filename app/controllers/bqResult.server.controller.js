a027f1db9709ef263a8cee3c4e5baa4359a9d42e
Leonid Enov <eleon16@mail.ru>
2017-10-20 18:44:17 +0300

    Database architecture changed

--- app/controllers/bqResult.server.controller.js
+++ app/controllers/bqResult.server.controller.js
@@ -25,71 +25,107 @@ const bigquery = require('@google-cloud/bigquery')({
     keyFilename: 'config/keys/mdma-17fcdb829378.json'
 });
 
-let dataSource = [
-        ga: {
-            id: 'ga',
-            name: 'Google Analytics',
-            content: {
-                dimension: ['industry', 'client', 'site', 'date', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'device_category'],
-                metrics: ['visits', 'pageviews', 'bounces', 'session_duration']
-            },
-            chosen: {
-                dimension: [],
-                metrics: [],
-                goals: 'no'
-            }
-        },
-        ym: {
-            id: 'ym',
-            name: 'Yandex Metrika',
-            content: {
-                dimension: ['industry', 'client', 'site', 'date', 'isBounce', 'time_on_site', 'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'device_category', 'gender', 'age'],
-                metrics: ['visits', 'users', 'pageviews']
-            },
-            chosen: {
-                dimension: [],
-                metrics: [],
-                goals: 'no'
-            }
-        },
-        postbuy: {
-            id: 'postbuy',
-            name: 'Postbuy',
-            content: ['Test1', 'Test2', 'Test3', 'Test4'],
-            chosen: []
-        }
+// Object with filter params
+
+let answer = {
+    "ga": {
+        "dimension": ["industry", "utm_source"],
+        "goals": "no",
+        "metrics": ["visits", "pageviews"]
     },
-    campaign: {
-        id: 'campaign',
-        name: 'Кампании',
-        content: [],
-        chosen: []
+    "ym": {
+        "dimension": ["time_on_site", "utm_campaign"],
+        "goals": "no",
+        "metrics": ["pageviews", "users"]
     },
-    placement: {
-        id: 'placement',
-        name: 'Размещение',
-        content: [],
-        chosen: []
-    },
-    medium: {
-        id: 'medium',
-        name: 'Medium',
-        content: [],
-        chosen: []
-    },
-    format: {
-        id: 'format',
-        name: 'Формат',
-        content: [],
-        chosen: []
-    },
-    sites: {
-        id: 'sites',
-        name: 'Сайты',
-        content: [],
-        chosen: []
+    "postbuy": ["fact_impressions", "fact_video_view_25", "fact_video_view_100", "fact_video_view_75", "fact_video_view_50"],
+    "filters": {
+        "industry": ["ecommerce","FMCG"],
+        "client": ["Karcher","Pepsico"],
+        "medium": ["cpm", "cpa"],
+        "placement": ["yahoo", "OTM", "twitter", "DBM", "mail"],
+        "campaign": ["lightbulb", "credits", "kasko", "molochko"],
+        "format": ["banner", "search", "video_120", "video_45"]
     }
-];
+};
+// Composing SELECT clause for GA or YM
+let selectConfig = (datasource) => {
+
+    let selectClause = "SELECT ";
+    for (let key in datasource.dimension) {
+        selectClause += datasource.dimension[key] + ", ";
+    }
+    for (let key in datasource.metrics) {
+        if (selectClause < datasource.metrics.length - 1) {
+            selectClause += "SUM(" + datasource.metrics[key] + ") AS " + datasource.metrics[key] + ", ";
+        } else {
+            selectClause += "SUM(" + datasource.metrics[key] + ") AS " + datasource.metrics[key];
+        }
+    }
+    return selectClause;
+}
+
+// Composing SELECT clause for postbuy
+
+let postbuySelectConfig = () => {
+
+    let selectClause = "SELECT ";
+    for (let key in answer.postbuy) {
+		if (selectClause === "SELECT ") {
+        	selectClause += answer.postbuy[key] + ", ";
+        } else {
+			selectClause += answer.postbuy[key];
+        }
+    }
+    return selectClause;
+}
+
+
+// Composing FROM clause ДОДЕЛАТЬ ДОДЕЛАТЬ ДОДЕЛАТЬ ДОДЕЛАТЬ => использовать TABLE_QUERY(dataset, expr)
+// (TABLE_QUERY([mdma-175510:google_analytics],  "REGEXP_MATCH(table_id, r'_gEnergy_') AND CAST(REGEXP_EXTRACT(table_id, '.*_(20.*)') AS INTEGER) < 20170515 AND CAST(REGEXP_EXTRACT(table_id, '.*_(20.*)') AS INTEGER) >= 20170513"))
+ let fromClause = "";
+
+var fromConfig = (datasource, dateFrom, dateTo) => {
+    switch (datasource) {
+        case "postbuy": "[mdma-175510:postbuy.all]";
+    break;
+        case "ga" : "[mdma-175510:google_analytics.Analytics_" + "Industry" + "Client" + "Site";
+    break;
+        case "ym" : "test3";
+    break;
+                      };
+};
+
+// Composing WHERE clause
+let whereConfig = () => {
+    let whereClause = "";
+    for (let key in answer.filters) {
+        if (whereClause === "") {
+            whereClause += ' WHERE (';
+        } else {
+            whereClause += 'AND (';
+        }
+        for (let i = 0; i < answer.filters[key].length; i++) {
+            if (i != 0) {
+                whereClause += 'OR ';
+            }
+            whereClause += key + ' CONTAINS "' + answer.filters[key][i];
+            if (i == answer.filters[key].length - 1) {
+                whereClause += '") ';
+            } else {
+                whereClause += '" ';
+            }
+        };
+    };
+    return whereClause;
+}
+// Composing ORDER BY clause
+
+let orderByClause = "";
+
+// Compose query
+
+let queryReq = selectClause + fromClause + whereClause
 
 // Function to compose query to BQ, send it and receive results
 
